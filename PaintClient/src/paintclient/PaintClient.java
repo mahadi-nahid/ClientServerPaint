75399e16df415da722c45050dc8f395ff0ba97c5
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-11-02 23:35:29 -0800

    Client Paint Creted

---
+++ PaintClient/src/paintclient/PaintClient.java
@@ -0,0 +1,17 @@
+
+package paintclient;
+
+/**
+ *
+ * @author MdMahadiHasan
+ */
+public class PaintClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
